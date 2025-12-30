00f791bae414c38fd1d7fbfca7dc4307c57f674b
Matthieu Khairallah <matthieu.khairallah@gmail.com>
2025-12-30 08:43:23 +0100

    added new query to fetch feed follows for a given user and auto-generated Go code equivalent with sqlc

---
+++ sql/queries/getFeedFollowForUser.sql
@@ -0,0 +1,10 @@
+-- name: GetFeedFollowsForUser :many
+SELECT 
+    f.name AS feed_name,
+    u.name AS user_name
+FROM 
+    users u
+    INNER JOIN feed_follows ff ON ff.user_id = u.id 
+    INNER JOIN feeds f ON f.id = ff.feed_id
+WHERE 
+    u.id = $1;
