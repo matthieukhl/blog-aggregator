aaff78a3709f4a93b7a5cddb3e4a630218db08de
Matthieu Khairallah <matthieu.khairallah@gmail.com>
2025-12-31 10:21:49 +0100

    updated createPost query to ignore duplicate keys on posts.url field

--- internal/database/createPost.sql.go
+++ internal/database/createPost.sql.go
@@ -17,7 +17,9 @@ const createPost = `-- name: CreatePost :one
 INSERT INTO posts (id, title, url, description, published_at, feed_id, created_at, updated_at)
 VALUES (
     $1, $2, $3, $4, $5, $6, $7, $8
-) RETURNING id, title, url, description, published_at, feed_id, created_at, updated_at
+) 
+ON CONFLICT (url) DO NOTHING
+RETURNING id, title, url, description, published_at, feed_id, created_at, updated_at
 `
 
 type CreatePostParams struct {
--- sql/queries/createPost.sql
+++ sql/queries/createPost.sql
@@ -2,4 +2,6 @@
 INSERT INTO posts (id, title, url, description, published_at, feed_id, created_at, updated_at)
 VALUES (
     $1, $2, $3, $4, $5, $6, $7, $8
-) RETURNING *;
+) 
+ON CONFLICT (url) DO NOTHING
+RETURNING *;
